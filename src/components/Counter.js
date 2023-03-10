cc42d708b9875ccdce9f24359ab1aa8384afbafa
Unam <97320894+manukao@users.noreply.github.com>
2023-03-10 11:14:11 +0100

    Initial commit

---
+++ src/components/Counter.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import ColoredNumber from "./ColoredNumber";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="counter">
+      <h1>Colored Number</h1>
+      <ColoredNumber value={count} />
+      <div className="counter__buttons-container">
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="increment count"
+          onClick={incrementCount}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="decrement count"
+          onClick={decrementCount}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
